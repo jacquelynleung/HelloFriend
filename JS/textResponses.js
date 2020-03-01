144ddabaa95c396bd91e25a6923cfdafb41134fd
Mallory Bridge <43453083+mmb2582@users.noreply.github.com>
2020-03-01 06:06:52 -0500

    Adding TEXT RESPONSES

---
+++ JS/textResponses.js
@@ -0,0 +1,5 @@
+let level1Sad = ["Do you want me to dance?", "Do you want me to tell you a joke?", "I hope your day is great"];
+
+let studentHealthCenter = "Student Health Center. Call at 585-475-2255. Their website is wellnessportal.rit.edu";
+
+let counselingAndPsy = "Counseling and Psychological Services. Call at 585-475-2261. Their website is caps@rit.edu";
