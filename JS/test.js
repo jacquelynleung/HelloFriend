b511aa5a283c6e86a4b0d88ea68da7784dea3797
Mallory Bridge <43453083+mmb2582@users.noreply.github.com>
2020-03-01 03:23:52 -0500

    our friend can dance

--- JS/test.js
+++ JS/test.js
@@ -1,3 +1,26 @@
+var isOn;
+isOn = "false";
+document.getElementById('chatter').style.display = 'none';
 function change() {
-    document.getElementById('wakeUp').src="/Images/HelloOnBlue.png"
+    if (isOn === "false") {
+        document.getElementById('wakeUp').src = "Images/HelloOnBlue.png";
+        document.getElementById('sleeping').style.display = 'none';
+        document.getElementById('chatter').style.display = 'grid';
+        isOn = "true";
+    } else{
+        document.getElementById('wakeUp').src = "Images/HelloOffBlue.png";
+        document.getElementById('sleeping').style.display = 'block';
+        document.getElementById('chatter').style.display = 'none';
+        isOn = "false";
+    }
+}
+
+function makeDance(){
+    var isDance = document.getElementById('message').value;
+    console.log(isDance);
+    if (isDance.includes('dance')){
+        console.log("should dance");
+        document.getElementById('friend').src = "Images/dancer.gif";
+    }
+
 }
